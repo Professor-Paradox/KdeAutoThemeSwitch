0f754aa9b6de1484a9c5b78d345a3f6fcb1ca4af
Professor-Paradox <anonymoustechiet@pm.me>
2020-07-17 20:31:53 +0530

    added currentthemedata function with related functions and code

--- SystemData.py
+++ SystemData.py
@@ -1,78 +1,30 @@
 import os,re
 
 homeDir=os.path.expanduser("~")
-testfile=open("test.txt","a+")
 
-# gtktheme,kvantum,cursor,gtkcursor,taskswitcher
-# directory location starting and ending with / is needed.
+# def main():
 
-# lookAndFeel
-# userThemes
-lookUserThemes=userDirFiles("/.local/share/plasma/look-and-feel/")
-# print(lookUserThemes)
-# systemThemes
-lookSystemThemes=systemDirFiles("/usr/share/plasma/look-and-feel/")
-# print(lookSystemThemes)
+   # gtktheme,kvantum,cursor,gtkcursor,taskswitcher
+   # directory location starting and ending with / is needed.
+   # print("hello")
 
-# plasmaTheme
-# userThemes
-plasmaUserThemes=userDirFiles("/.local/share/plasma/desktoptheme/")
-# print(plasmaUserThemes)
-# systemThemes
-plasmaSystemThemes=systemDirFiles("/usr/share/plasma/desktoptheme/")
-# print(plasmaSystemThemes)
+# takes absolute userpath,needs userPath value to inclue backslash,ex: /userpath/
+def userDirFiles(userPath):
+   x = os.listdir(f'{homeDir}{userPath}')
+   x.sort()
+   return x
 
-# applicationStyles
-# systemThemes
-applicationSystemThemes=systemDirFiles("/usr/lib/x86_64-linux-gnu/qt5/plugins/styles/")
-# print(applicationSystemThemes)
+# takes absolute system path
+def systemDirFiles(systemPath):
+   x = os.listdir(f'{systemPath}')
+   x.sort()
+   return x
 
-# windowdecoratios
-# userThemes
-windowUserThemes=userDirFiles("/.local/share/aurorae/themes/")
-# print(windowUserThemes)
-# systemThemes
-windowSystemThemes=systemDirFiles("/usr/share/kwin/decorations/")
-# print(windowSystemThemes)
-
-# colorScheme
-# userThemes
-colorUserThemes = userDirFiles("/.local/share/color-schemes/color-schemes/")
-# print(colorUserThemes)
-# systemThemes
-colorSystemThemes = systemDirFiles("/usr/share/color-schemes/")
-# print(colorSystemThemes)
-
-# icons
-# userThemes
-iconsUserThemes = userDirFiles("/.local/share/icons/")
-# print(iconsUserThemes)
-# systemThemes
-iconsSystemThemes = systemDirFiles("/usr/share/icons/")
-# print(iconsSystemThemes)
-
-# splashScreen
-# userThemes
-splashUserThemes = userDirFiles("/.local/share/plasma/look-and-feel/")
-# print(splashUserThemes)
-# systemThemes
-splashSystemThemes = systemDirFiles("/usr/share/plasma/look-and-feel/")
-# print(splashSystemThemes)
-
-# TestData
-# testlist=[
-#  'abcd.colors',
-#  'abcd.colors.something',
-#  'abcd.so',
-#  'abcd.so.something',
-#  'abcd.png',
-#  'abcd.png.something',
-#  'abcd.json',
-#  'abcd.json.something',
-#  'abcd.desktop',
-#  'abcd.desktop.something'
-# ]
-# testlist.sort()
+# writes output to a file given above,one item per line
+def append(alist):
+   testfile=open("test.txt","a+")
+   for i in alist:
+      testfile.write(f"{i}\n")
 
 # here a list of values are passed,
 # on deleteLine true entire line is deleted,
@@ -95,34 +47,224 @@ def editListValues(alist,deleteLine=False,*regexValue):
    # print(tempList)
    return tempList
 
-# takes absolute userpath,needs userPath value to inclue backslash,ex: /userpath/
-def userDirFiles(userPath):
-   x = os.listdir(f'{homeDir}{userPath}')
-   x.sort()
-   return x
+def organizeData(userData,SystemData):
+   tempList = []
 
-# takes absolute system path
-def systemDirFiles(systemPath):
-   x = os.listdir(f'{systemPath}')
-   x.sort()
-   return x
+   tempList.append('==========')
+   tempList.append('User Theme')
+   tempList.append('==========')
+   for x in userData:
+      tempList.append(x)
 
-# writes output to a file given above,one item per line
-def append(alist):
-   for i in alist:
-      testfile.write(f"{i}\n")
+   tempList.append('==========')
+   tempList.append('System Theme')
+   tempList.append('==========')
+   for x in SystemData:
+      tempList.append(x)
+   
+   return tempList
+
+def lookAndFeel():
+   # lookAndFeel
+   # userThemes
+   UserThemes = userDirFiles("/.local/share/plasma/look-and-feel/")
+   # print(lookUserThemes)
+   # systemThemes
+   SystemThemes = systemDirFiles("/usr/share/plasma/look-and-feel/")
+   # print(lookSystemThemes)
+   editedUserList=editListValues(UserThemes,True,'.json')
+   editedSystemList=editListValues(SystemThemes,False,'.desktop')
+   return organizeData(editedUserList,editedSystemList)
+
+def plasmaTheme():
+   # plasmaTheme
+   # userThemes
+   UserThemes=userDirFiles("/.local/share/plasma/desktoptheme/")
+   # print(plasmaUserThemes)
+   # systemThemes
+   SystemThemes=systemDirFiles("/usr/share/plasma/desktoptheme/")
+   # print(plasmaSystemThemes)
+   editedUserList=editListValues(UserThemes,True,'.json')
+   editedSystemList=editListValues(SystemThemes)
+   return organizeData(editedUserList,editedSystemList)
+
+def applicationStyles():
+   # applicationStyles
+   # systemThemes
+   SystemThemes=systemDirFiles("/usr/lib/x86_64-linux-gnu/qt5/plugins/styles/")
+   # print(applicationSystemThemes)
+   editedSystemList=editListValues(SystemThemes,False,'.so')
+   return organizeData([],editedSystemList)
+
+def windowDecorations():
+   # windowdecoratios
+   # userThemes
+   UserThemes=userDirFiles("/.local/share/aurorae/themes/")
+   # print(windowUserThemes)
+   # systemThemes
+   SystemThemes=systemDirFiles("/usr/share/kwin/decorations/")
+   # print(windowSystemThemes)
+   editedUserList=editListValues(UserThemes)
+   editedSystemList=editListValues(SystemThemes)
+   return organizeData(editedUserList,editedSystemList)
+
+def gtkTheme():
+   pass
+def colorScheme():
+   # colorScheme
+   # userThemes
+   UserThemes = userDirFiles("/.local/share/color-schemes/color-schemes/")
+   # print(colorUserThemes)
+   # systemThemes
+   SystemThemes = systemDirFiles("/usr/share/color-schemes/")
+   # print(colorSystemThemes)
+   editedUserList=editListValues(UserThemes,False,'.colors')
+   editedSystemList=editListValues(SystemThemes,False,'.colors')
+   return organizeData(editedUserList,editedSystemList)
+
+def icons():
+   # icons
+   # userThemes
+   UserThemes = userDirFiles("/.local/share/icons/")
+   # print(iconsUserThemes)
+   # systemThemes
+   SystemThemes = systemDirFiles("/usr/share/icons/")
+   # print(iconsSystemThemes)
+   editedUserList=editListValues(UserThemes,True,'.png')
+   editedSystemList=editListValues(SystemThemes,True,'.png')
+   return organizeData(editedUserList,editedSystemList)
+
+def kvantum():
+   pass
+def splashScreen():
+   # splashScreen
+   # userThemes
+   UserThemes = userDirFiles("/.local/share/plasma/look-and-feel/")
+   # print(splashUserThemes)
+   # systemThemes
+   SystemThemes = systemDirFiles("/usr/share/plasma/look-and-feel/")
+   # print(splashSystemThemes)
+   editedUserList=editListValues(UserThemes,True,'.json')
+   editedSystemList=editListValues(SystemThemes,False,'.desktop')
+   return organizeData(editedUserList,editedSystemList)
+
+  # calling the method with list,and regex
+   # have to pass exact value example .so will remove .so,so will remove only so
+   # x = editListValues(lookUserThemes,True,'.json')
+   # for i in x:
+   #    print(i)
+
+   # editListValues(lookUserThemes)
+   # append(lookSystemThemes)
+   # append(plasmaUserThemes)
+   # append(plasmaSystemThemes)
+   # append(applicationSystemThemes)
+   # append(windowUserThemes)
+   # append(windowSystemThemes)
+
+# TestData
+# innerList=[
+#  'abcd.colors',
+#  'abcd.colors.something',
+#  'abcd.so',
+#  'abcd.so.something',
+#  'abcd.png',
+#  'abcd.png.something',
+#  'abcd.json',
+#  'abcd.json.something',
+#  'abcd.desktop',
+#  'abcd.desktop.something'
+# ]
+# innerList.sort()
+
+# this method gets data from other methods easy to implement.
+def getSystemData(themeName):
+   return themeName()
 
 
-# calling the method with list,and regex
-# have to pass exact value example .so will remove .so,so will remove only so
-# x = editListValues(lookUserThemes,False,'.so')
-# for i in x:
-   # print(i)
+# take data from a file line by line,then add it to a list,create list of lists,reset the innerList 
+def listFromFile(fileName):
+   outerList=[]
+   # read data from a file
+   with open(f'{homeDir}{fileName}') as f:
+      innerList=[]
+      # appending each line to innerList,then from innerList to outerList
+      for i in f.readlines():
+         if i == "\n":#empty line means a new group of data
+            outerList.append(innerList)
+            innerList=[] # reset list to keep new group of data
+         else:
+            innerList.append(i)
+      # for loop breaks when EOF reached,then this will append the last list
+      outerList.append(innerList)
+   return outerList
 
-# editListValues(lookUserThemes)
-# append(lookSystemThemes)
-# append(plasmaUserThemes)
-# append(plasmaSystemThemes)
-# append(applicationSystemThemes)
-# append(windowUserThemes)
-# append(windowSystemThemes)
+
+# a function to extract data from lists.first value is dictionary name
+def dictionaryFromList(inputList):
+   dictionaryData=[]
+
+   for i in inputList: # two dimensional lists,so two for loops
+      tempDictionaryData=[]#inner list gets reset every time loop starts
+      for j in i:
+         # extracting the Dictionaryname i.e, group(KDE) name,to create a dictionary
+         dictionaryName=(re.findall('\[.*]*',j))
+         if not dictionaryName == []: # we append non-empty data into a list
+            # to apply regex on a list, we need to convert it to string or use it's index
+            temp=re.sub('\[','',dictionaryName[0])
+            dictionaryName=re.sub(']','',temp)
+            tempDictionaryData.append(dictionaryName)
+       #########################################     
+         # extracting dictionary Data
+         temp=(re.sub('\n','',j))
+         dicData= (re.findall('^\w.*',temp))#taking data,which usually starts with a word
+         if not dicData == []:
+            tempDictionaryData.append(dicData[0])
+         else:
+            #there is a line break for every group of data,when a line break is encountered we save data into actual dictionary
+            dictionaryData.append(tempDictionaryData)
+   
+   return dictionaryData
+
+# function that will output a dictionaries from a file.
+def getDictionaryFromFile(inputFile):
+   # calling other function to get data
+   receivedList=dictionaryFromList(listFromFile(inputFile))
+   listOfDictionaries=[]
+   # we receive a list of lists,where each list is a group of data,that we convert to a dictionary
+   for i in receivedList:
+      # a dictionary to store in above list
+      innerDict={}
+      for j in i:#j is the value inside a list
+         # not all elements have equalto symbol inthem,so had to take this approach of individual key,value pair
+         key=j.split('=')[0]
+         val=j.split('=')[-1]
+         if key==val:
+            # in this list we have one value,with no =,so we give it our own.
+            pair={"dictionaryName":val}
+            innerDict.update(pair)
+         else:
+            # adding other key-value pairs into the list
+            pair={key:val}
+            innerDict.update(pair)
+      # creating a list of dictionaries
+      listOfDictionaries.append(innerDict.copy())
+   return listOfDictionaries      
+# getDictionaryFromFile('/.config/kdeglobals')
+# print(dictionaryFromList(listFromFile('/.config/kcminputrc')))
+
+# returns the value for given theme file from a groupname
+def getCurrentThemeInfo(inputFile,groupName,key):
+   dictionary=getDictionaryFromFile(inputFile)
+   for i in dictionary:
+      if i.get('dictionaryName') == groupName:
+         return i.get(key)
+
+
+
+
+
+
+# if __name__ == "__main__":
+#    main()
+# currentThemeData()
--- UiControl.py
+++ UiControl.py
@@ -1,5 +1,6 @@
 from plasma import Ui_MainWindow
 import sys
+import SystemData as SD
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QVBoxLayout
 
@@ -16,17 +17,30 @@ class MyWindow(QtWidgets.QMainWindow):
       # calls given function on button Click(UI),by default doesn't accept any parameters.
       # we can use lambda to pass extra parameters,below line is an example
       # self.ui.checkBox.toggled.connect(lambda: self.toggleWidget("hello"))
-
+      # print(SystemData.userDirFiles('/.local/share/plasma/look-and-feel/'))
+      
       # linking relevant grid elements with checkbox
-      self.ui.checkBox.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout))
-      self.ui.checkBox2.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout2))
-      self.ui.checkBox3.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout3))
-      self.ui.checkBox4.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout4))
-      self.ui.checkBox5.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout5))
-      self.ui.checkBox6.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout6))
-      self.ui.checkBox7.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout7))
-      self.ui.checkBox8.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout8))
-      self.ui.checkBox9.toggled.connect(lambda: self.toggleWidget(self.ui.gridLayout9))
+      self.ui.checkBox.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox,self.ui.gridLayout,SD.lookAndFeel))
+      self.ui.checkBox2.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox2,self.ui.gridLayout2,SD.plasmaTheme))
+      self.ui.checkBox3.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox3,self.ui.gridLayout3,SD.applicationStyles))
+      self.ui.checkBox4.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox4,self.ui.gridLayout4,SD.windowDecorations))
+      self.ui.checkBox5.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox5,self.ui.gridLayout5,SD.gtkTheme))
+      self.ui.checkBox6.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox6,self.ui.gridLayout6,SD.colorScheme))
+      self.ui.checkBox7.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox7,self.ui.gridLayout7,SD.icons))
+      self.ui.checkBox8.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox8,self.ui.gridLayout8,SD.kvantum))
+      self.ui.checkBox9.toggled.connect(
+         lambda: self.toggleWidget(self.ui.checkBox9,self.ui.gridLayout9,SD.splashScreen))
+      x=SD.getCurrentThemeInfo('/.config/gtk-3.0/settings.ini','Settings','gtk-cursor-theme-name')
+      print(x)
+
 
    # disable all widgets
    def disableAll(self):
@@ -41,15 +55,19 @@ class MyWindow(QtWidgets.QMainWindow):
       self.enableWidgets(self.ui.gridLayout9)
 
    # when clicked switches on all elements inside the box.
-   def toggleWidget(self,gridLayout):
-      # taking the gridlayout as innerlayout and passing them to enablewidgets which will switch them on/off
-      if (self.ui.checkBox.isChecked() == True):
+   def toggleWidget(self,checkBoxValue,gridLayout,themeName):
+      # using given checkbox to trigger the given gridlayout
+      # passing them to enablewidgets which will switch them on/off
+      if (checkBoxValue.isChecked() == True):
          # switches on elements
          self.enableWidgets(gridLayout,True)  
-
-      if (self.ui.checkBox.isChecked() == False):
+         x=SD.getSystemData(themeName)
+         print(x)
+      else:
+      # if (self.ui.checkBox.isChecked() == False):
          # switches off elements
          self.enableWidgets(gridLayout)
+         print("bye")
 
    # this function takes a gridlayout to enable/disable widgets in it.
    def enableWidgets(self,gridLayout,isEnable=False):
@@ -64,7 +82,13 @@ class MyWindow(QtWidgets.QMainWindow):
          # temp=gridLayout.itemAt(i).widget().objectName()
          # print(temp)
          # here exec is used to execute on a edited string,raises no errors
+         # code in the brackets gets the widget name,which would be enabled
          exec(f"self.ui.{gridLayout.itemAt(i).widget().objectName()}.setEnabled(isEnable)")
+         
+   
+         # fetching data from SystemData class,passing it to comboBoxe items.
+
+
       
 
 # main method used to call when file is being executed.
@@ -78,8 +102,30 @@ def main():
 if __name__ == "__main__":
    main()
 
-
-# TODO function to fetch data from SystemData
+# lookAndFeel
+# print(SD.getCurrentThemeInfo('/.config/kdeglobals','KDE','LookAndFeelPackage'))
+# plasmaTheme
+# print(SD.getCurrentThemeInfo('/.config/plasmarc','Theme','name'))
+# applicationStyleEdit
+# print(SD.getCurrentThemeInfo('/.config/kdeglobals','org.kde.kdecoration2','theme'))
+# windowDecoration
+# print(SD.getCurrentThemeInfo('/.config/kwinrc','org.kde.kdecoration2','theme'))
+# gtkTheme
+# print(SD.getCurrentThemeInfo('/.config/gtk-3.0/settings.ini','Settings','gtk-theme-name'))
+# colorScheme
+# print(SD.getCurrentThemeInfo('/.config/kdeglobals','General','ColorScheme'))
+# icons
+# print(SD.getCurrentThemeInfo('/.config/kdeglobals','Icons','Theme'))
+# cursor
+# print(SD.getCurrentThemeInfo('/.config/kcminputrc','Mouse','cursorTheme'))
+# gtkCursor
+# print(SD.getCurrentThemeInfo('/.config/gtk-3.0/settings.ini','Settings','gtk-cursor-theme-name'))
+# kvantumTheme
+# print(SD.getCurrentThemeInfo('/.config/Kvantum/kvantum.kvconfig','General','theme'))
+# splashScreen
+# print(SD.getCurrentThemeInfo('/.config/ksplashrc','KSplash','Theme'))
+# alt-tab theme
+# print(SD.+getCurrentThemeInfo('/.config/kwinrc','TabBox','LayoutName'))
 # TODO function to push data to drop downs
-# TODO function to check the current theme data being used,from the kde files in theme.sh file
 # TODO function to create a file with user given data,from save button.
+# TODO check rescue time completely,sync everything and create a list of options to create.
